11f6baa3d4a16359ada344cc57f15779927caeca
j1nxie <rylieeeeexd@gmail.com>
2024-04-05 01:19:43 +0700

    feat(status): display poise version

--- src/commands/status.rs
+++ src/commands/status.rs
@@ -1,6 +1,9 @@
 use std::time::UNIX_EPOCH;
 
-use crate::{constants::version::get_version, Context, Error};
+use crate::{
+    constants::{version::get_version, POISE_VERSION},
+    Context, Error,
+};
 use poise::serenity_prelude as serenity;
 
 fn get_bot_avatar(ctx: Context<'_>) -> String {
@@ -12,9 +15,10 @@ fn get_bot_avatar(ctx: Context<'_>) -> String {
 pub async fn status(ctx: Context<'_>) -> Result<(), Error> {
     ctx.send(poise::CreateReply::default().embed(
         serenity::CreateEmbed::new()
-        .field("about the bot", "[tairitsu](https://github.com/j1nxie/tairitsu-rs) is a Discord bot for Arcaea, written by [j1nxie](https://github.com/j1nxie).", false)
+        .field("about the bot", "[tairitsu](https://github.com/j1nxie/tairitsu-rs) is a Discord bot for Arcaea, written by [j1nxie](https://github.com/j1nxie), using the [poise](https://github.com/serenity-rs/poise) framework.", false)
         .field("version", get_version(), false)
         .field("rust", format!("[{0}](https://releases.rs/docs/{0})", rustc_version_runtime::version().to_string()), true)
+        .field("poise", format!("[{0}](https://docs.rs/crate/poise/{0})", POISE_VERSION), true)
         .field("uptime", format!("<t:{}:R>", std::time::SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs()), true)
         .thumbnail(get_bot_avatar(ctx))
     )).await?;
--- src/constants/mod.rs
+++ src/constants/mod.rs
@@ -1,3 +1,4 @@
 pub mod version;
 
 pub const API_URL: &str = "https://webapi.lowiro.com/webapi/user/me";
+pub const POISE_VERSION: &str = "0.6.1";
