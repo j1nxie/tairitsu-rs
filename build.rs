c5da7f39f2c3abbdbb6d3df173a6b00468db9095
j1nxie <rylieeeeexd@gmail.com>
2024-04-08 16:16:13 +0700

    chore: clean up leftover documentation from sample code

--- build.rs
+++ build.rs
@@ -2,8 +2,6 @@ use anyhow::Error;
 use vergen::EmitBuilder;
 
 pub fn main() -> Result<(), Error> {
-    // NOTE: This will output everything, and requires all features enabled.
-    // NOTE: See the EmitBuilder documentation for configuration options.
     EmitBuilder::builder().git_sha(true).emit()?;
     Ok(())
 }
